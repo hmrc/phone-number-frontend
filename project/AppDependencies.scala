2979c318f9842cc6ff98ee2f01d7a0c1f1638746
Saqib Saleem <64003679+ssaleem-ee@users.noreply.github.com>
2023-07-06 13:45:57 +0100

    TAV-1070: Fix bobby violations (#31)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,19 +1,17 @@
-import play.core.PlayVersion
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
-import sbt._
+import play.sbt.PlayImport.*
+import sbt.*
 
 object AppDependencies {
-  val hmrcBootstrapVersion = "7.2.0"
+  val hmrcBootstrapVersion = "7.19.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % hmrcBootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "3.19.0-play-28"
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "7.14.0-play-28"
   )
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % hmrcBootstrapVersion % "test, it",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % "0.71.0"             % IntegrationTest,
-    "org.mockito"            %% "mockito-scala"            % "1.17.7"             % Test
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % "1.3.0"             % IntegrationTest,
+    "org.mockito"            %% "mockito-scala"            % "1.17.12"             % Test
   )
 }
