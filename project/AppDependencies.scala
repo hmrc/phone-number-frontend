bb95d4f64693ccb8b2ae2aff3fdb9f4aaadc285b
grahampaulcook <11269301+grahampaulcook@users.noreply.github.com>
2025-11-07 15:03:49 +0000

    CIR-2481 Use the new GOV.UK Service Navigation component

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlayVersion = "10.1.0"
+  private val bootstrapPlayVersion = "10.4.0"
   private val playSuffix = "-play-30"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% s"bootstrap-frontend$playSuffix" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% s"play-frontend-hmrc$playSuffix" % "12.8.0"
+    "uk.gov.hmrc" %% s"play-frontend-hmrc$playSuffix" % "12.20.0"
   )
 
   val test: Seq[ModuleID] = Seq(
