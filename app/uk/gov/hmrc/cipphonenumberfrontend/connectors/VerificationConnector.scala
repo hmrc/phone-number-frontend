f9d36ea1f5fc6b8071fc391ccf7334d9429aae7a
ellie-l-newell <113107044+ellie-l-newell@users.noreply.github.com>
2026-01-22 12:13:02 +0000

    [CIR-2355] - Update to Scala 3

--- app/uk/gov/hmrc/cipphonenumberfrontend/connectors/VerificationConnector.scala
+++ app/uk/gov/hmrc/cipphonenumberfrontend/connectors/VerificationConnector.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,12 +17,13 @@
 package uk.gov.hmrc.cipphonenumberfrontend.connectors
 
 import play.api.libs.json.Json
+import play.api.libs.ws.JsonBodyWritables.writeableOf_JsValue
 import uk.gov.hmrc.cipphonenumberfrontend.config.AppConfig
 import uk.gov.hmrc.cipphonenumberfrontend.models.{
   PhoneNumber,
   PhoneNumberAndPasscode
 }
-import uk.gov.hmrc.http.HttpReads.Implicits._
+import uk.gov.hmrc.http.HttpReads.Implicits.*
 import uk.gov.hmrc.http.client.HttpClientV2
 import uk.gov.hmrc.http.{
   HeaderCarrier,
