e606307da8ad3c6ec88f0155c25a21a43506f7b1
ellie-l-newell <113107044+ellie-l-newell@users.noreply.github.com>
2026-01-22 12:31:33 +0000

    [CIR-2355] - Addressed PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,16 +2,16 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlayVersion = "10.4.0"
+  private val bootstrapPlayVersion = "10.5.0"
   private val playSuffix = "-play-30"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% s"bootstrap-frontend$playSuffix" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% s"play-frontend-hmrc$playSuffix" % "12.20.0"
+    "uk.gov.hmrc" %% s"play-frontend-hmrc$playSuffix" % "12.27.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% s"bootstrap-test$playSuffix"   % bootstrapPlayVersion,
-    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test$playSuffix"  % "2.10.0"
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test$playSuffix"  % "2.11.0"
   ).map(_ % Test)
 }
